6459874261726beb3bad2e5067c0c965314b7864
Shivam Agarwal <itisshivam22@gmail.com>
2023-08-13 00:37:20 +0530

    feat: add more action blocks

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,19 +3,52 @@ import { Droppable } from "react-beautiful-dnd";
 import ActionBlock from "./ActionBlock";
 
 export default function Sidebar({ actionBlocks }) {
+  const getActionBlocks = (blockType) => {
+    return actionBlocks.map((blockInfo, index) => (
+      blockInfo.blockType === blockType && <ActionBlock blockInfo={blockInfo} index={index} key={blockInfo.blockid} />
+    ));
+  };
   return (
     <div className="w-60 flex-none h-full overflow-y-auto flex flex-col items-start p-2 border-r border-gray-200">
       <div className="font-bold"> {"Events"} </div>
       <Droppable droppableId="sidebar" type="ACTION_BLOCK">
         {(provided) => (
           <div ref={provided.innerRef} {...provided.droppableProps}>
-            {actionBlocks.map((blockInfo, index) => (
-              <ActionBlock blockInfo={blockInfo} index={index} key={blockInfo.blockid} />
-            ))}
+            {getActionBlocks("event")}
             {provided.placeholder}
           </div>
         )}
       </Droppable>
+
+      <div className="font-bold"> {"Motion"} </div>
+      <Droppable droppableId="sidebar" type="ACTION_BLOCK">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {getActionBlocks("motion")}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+
+      <div className="font-bold"> {"Looks"} </div>
+      <Droppable droppableId="sidebar" type="ACTION_BLOCK">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {getActionBlocks("looks")}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+
+      <div className="font-bold"> {"Control"} </div>
+      <Droppable droppableId="sidebar" type="ACTION_BLOCK">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {getActionBlocks("control")}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>  
     </div>
   );
 }
--- src/data/actionBlocks.js
+++ src/data/actionBlocks.js
@@ -13,18 +13,21 @@ const actionBlocksData = [
     blockColor: "bg-yellow-500",
     blockAction: () => {},
     blockid: "when_clicked",
+    blockType: "event",
   },
   {
     blockName: "When this sprite clicked",
     blockColor: "bg-yellow-500",
     blockAction: () => {},
     blockid: "when_this_sprite_clicked",
+    blockType: "event",
   },
   {
     blockName: "Move 10 steps",
     blockColor: "bg-blue-500",
     blockAction: () => {},
     blockid: "move_10_steps",
+    blockType: "motion",
   },
   {
     blockName: (
@@ -37,6 +40,7 @@ const actionBlocksData = [
     blockColor: "bg-blue-500",
     blockAction: () => {},
     blockid: "turn_15_degrees_undo",
+    blockType: "motion",
   },
   {
     blockName: (
@@ -49,6 +53,35 @@ const actionBlocksData = [
     blockColor: "bg-blue-500",
     blockAction: () => {},
     blockid: "turn_15_degrees_redo",
+    blockType: "motion",
+  },
+  {
+    blockName: "Show",
+    blockColor: "bg-purple-500",
+    blockAction: () => {},
+    blockid: "show",
+    blockType: "looks",
+  },
+  {
+    blockName: "Hide",
+    blockColor: "bg-purple-500",
+    blockAction: () => {},
+    blockid: "hide",
+    blockType: "looks",
+  },
+  {
+    blockName: "Wait 1 seconds",
+    blockColor: "bg-red-500",
+    blockAction: () => {},
+    blockid: "wait_1_seconds",
+    blockType: "control",
+  },
+  {
+    blockName: "Repeat 10 times",
+    blockColor: "bg-red-500",
+    blockAction: () => {},
+    blockid: "repeat_10_times",
+    blockType: "control",
   },
 ];
 
